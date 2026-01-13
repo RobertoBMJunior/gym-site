38cc57c5442e641fb98ca7d67fdea5b781b3479b
Roberto Junior <robertojuniorazul@gmail.com>
2026-01-13 02:22:39 -0300

    style: Ajustar cor de background do header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,22 +1,33 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header() {
-    return (
-        <header className="w-full h-20 flex justify-between items-center py-4 px-54 bg-zinc-300 text-slate-900">
-            <Link href="/" aria-label="Ir para página inicial">
-                <Image src="/logoBarbell.svg" alt="Logo" width={48} height={48} priority/>
-            </Link>
+  return (
+    <header className="w-full h-20 flex justify-between items-center py-4 px-54 bg-white/80 backdrop-blur text-slate-900 border-b border-slate-200">
+      <Link href="/" aria-label="Ir para página inicial">
+        <Image
+          src="/logoBarbell.svg"
+          alt="Logo"
+          width={48}
+          height={48}
+          priority
+        />
+      </Link>
 
-            <div className="flex gap-4 font-semibold">
-                <Link href="/" className="hover:bg-slate-200 px-2 py-1 rounded transition-colors duration-200">
-                    Como funciona
-                </Link>
-                <Link href="/" className="hover:bg-slate-200 px-2 py-1 rounded transition-colors duration-200">
-                    Benefícios
-                </Link>
-            </div>
-
-        </header>
-    )
+      <div className="flex gap-4 font-semibold">
+        <Link
+          href="/"
+          className="hover:bg-slate-200 px-2 py-1 rounded transition-colors duration-200"
+        >
+          Como funciona
+        </Link>
+        <Link
+          href="/"
+          className="hover:bg-slate-200 px-2 py-1 rounded transition-colors duration-200"
+        >
+          Benefícios
+        </Link>
+      </div>
+    </header>
+  )
 }
