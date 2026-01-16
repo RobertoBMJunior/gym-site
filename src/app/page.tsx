ad5ef4d3fe9be077659746e22370c4f29760048f
Roberto Junior <robertojuniorazul@gmail.com>
2026-01-15 23:57:50 -0300

    feat: Criar a seção Benefícios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import { Benefits } from '@/components/sections/Benefits'
 import { HeroSection } from '@/components/sections/HeroSection'
 import { HowItWorks } from '@/components/sections/HowItWorks'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <HowItWorks />
+      <Benefits />
     </div>
   )
 }
---
+++ src/components/sections/Benefits.tsx
@@ -0,0 +1,9 @@
+export function Benefits() {
+  return (
+    <section className="border-t border-slate-100">
+      <div className="max-w-7xl mx-auto px-6">
+        <h2 className="text-4xl font-bold text-center">Benefícios</h2>
+      </div>
+    </section>
+  )
+}
